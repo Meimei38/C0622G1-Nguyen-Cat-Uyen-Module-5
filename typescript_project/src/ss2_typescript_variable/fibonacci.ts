886c93476d8cc56257eba22d2adb5dcfa66cd5e8
Meimei38 <uyennguyencat@gmail.com>
2022-11-18 16:35:33 +0700

    update

---
+++ typescript_project/src/ss2_typescript_variable/fibonacci.ts
@@ -0,0 +1,10 @@
+function isFibonacci(number: number): number{
+    if (number ==0){
+        return 0;
+    }
+    if (number ==1){
+        return 1;
+    }
+    return isFibonacci(number -1) + isFibonacci(number-2);
+}
+console.log(isFibonacci(3));
