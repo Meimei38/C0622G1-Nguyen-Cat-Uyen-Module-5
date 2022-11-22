4d3fd4ebf571fdddc341cd23264ea8afc6eb6902
Meimei38 <uyennguyencat@gmail.com>
2022-11-22 14:25:52 +0700

    update

--- angular/exercise/src/app/app.module.ts
+++ angular/exercise/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import {FormsModule} from "@angular/forms";
 import { ColorPickComponent } from './color-pick/color-pick.component';
+import { ArticleComponent } from './article/article.component';
+import { LikeComponent } from './article/like/like.component';
+import { NavbarComponent } from './article/navbar/navbar.component';
+import { FooterComponent } from './article/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
-    ColorPickComponent
+    ColorPickComponent,
+    ArticleComponent,
+    LikeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
     imports: [
         BrowserModule,
